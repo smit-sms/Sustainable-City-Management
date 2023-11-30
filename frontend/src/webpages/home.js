78cb65893ffcfbbab09e56446abc8f1b70ffbc29
Smit Shah <smit16@somaiya.edu>
2023-11-30 23:56:12 +0000

    SCM-6: Added Basic Frontend using React (#3)
    * SCM-6: Added basic frontend using react
    
    * SCM-6: api url abstraction and updated settings.py in backend
    
    * SCM-6: Added github action tests
    
    * SCM-6: update github actions yaml

---
+++ frontend/src/webpages/home.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect }  from 'react';
+import { Link } from 'react-router-dom';
+import { API_URL } from './api';
+
+const Home = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [thinslices, setThinSlices] = useState([]);
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    console.log(data);
+                    setIsLoaded(true);
+                    setThinSlices(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {thinslices.map((thinslice, i) => (
+                    <div className={"row"} key={i} id={i}>
+                        <li>
+                            <Link to={`thinslice/${thinslice.id}`}>{thinslice.firstname}</Link>
+                        </li>
+                    </div>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default Home;
