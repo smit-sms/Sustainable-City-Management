773b59eaffcd56de8a0f58d0aa47f6332964878a
Gayathri Girish Nair <ggnair2000@gmail.com>
2024-01-25 18:02:26 +0000

    post and get

---
+++ SCM/Backend/sensors/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('pm2.5/', views.air_pm2_5),
+]
--- SCM/Backend/sensors/views.py
+++ SCM/Backend/sensors/views.py
@@ -5,7 +5,8 @@ import requests
 from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import datetime, timedelta
-from .views import Sensor, Air, Noise, DateTime
+from .models import Sensor, Air, Noise, DateTime
+from django.views.decorators.csrf import csrf_exempt
 
 sensors_air = [
     'DCC-AQ2', 'DCC-AQ3', 'DCC-AQ4', 
@@ -28,55 +29,61 @@ sensors_noise = [
 ]
 
 # Create your views here.
+@csrf_exempt
 def air_pm2_5(request):
     url_root = "https://data.smartdublin.ie/sonitus-api"
     data = {}
     if request.method == 'POST':
         request_json = json.loads(request.body.decode('utf-8'))
-        monitor = request_json['monitor']
-        if monitor not in sensors_air:
-            return JsonResponse({'bad request': 'Not a pm2.5 monitor.'}, status=400, safe=True)
+        sensor_serial_number = request_json['sensor_serial_number']
+        if sensor_serial_number not in sensors_air:
+            return JsonResponse({'message': 'Not a pm2.5 sensor.'}, status=400, safe=True)
+        
+        # Fetching data from now to 24 hrs ago.
         datetime_now = datetime.now()
         datetime_yesterday = datetime_now - timedelta(days=1)
         datetime_end = time.mktime(datetime_now.timetuple())
         datetime_start = time.mktime(datetime_yesterday.timetuple())
-
-        res = requests.post(f"{url_root}/api/data", json={
-            'username': "dublincityapi",
-            'password': "Xpa5vAQ9ki",
-            'monitor': monitor,
-            'start': datetime_start,
-            'end': datetime_end
-        })
+        print(datetime_start, datetime_end)
         try:
+            res = requests.post(f"{url_root}/api/data", json={
+                'username': "dublincityapi",
+                'password': "Xpa5vAQ9ki",
+                'monitor': sensor_serial_number,
+                'start': datetime_start,
+                'end': datetime_end
+            })
             if len(res.text) > 0: 
                 data_list = [{'datetime':d['datetime'], 'pm2_5':d['pm2_5']} for d in res.json()]
             else: data_list = []
         except Exception as e:
-            return JsonResponse({'error': 'Failed to fetch data.'}, status=403, safe=True)
+            return JsonResponse({'error': f'Failed to fetch data due to {e}'}, status=404, safe=True)
 
-        data["text"] = "Response to POST request."
         data["data"] = data_list
 
         # Add to the database.
-        db_monitor, db_monitor_created = Sensor.objects.get_or_create(serial_number=monitor)
-        for d in data_list:
-            dt = datetime.strptime(d['datetime'], "%Y-%m-%d %H:%M:%S")
-            dt = dt.replace(tzinfo=pytz.timezone('GMT'))
-            db_datetime, db_datetime_created = DateTime.objects.get_or_create(datetime=dt)
-            try:
-                db_pm2_5 = Air.objects.get(pm2_5=d['pm2_5'], datetime_id=db_datetime.id, monitor_id=db_monitor.id)
-            except Air.DoesNotExist:
-                db_pm2_5 = Air(pm2_5=d['pm2_5'], datetime_id=db_datetime.id, monitor_id=db_monitor.id)
-                db_pm2_5.save()
+        db_obj_sensor = Sensor.objects.filter(serial_number=sensor_serial_number).first()
+        if (db_obj_sensor != None):
+            for d in data_list:
+                dt = datetime.strptime(d['datetime'], "%Y-%m-%d %H:%M:%S")
+                dt = dt.replace(tzinfo=pytz.timezone('GMT'))
+                db_datetime, db_datetime_created = DateTime.objects.get_or_create(datetime=dt)
+                try:
+                    db_pm2_5 = Air.objects.get(pm2_5=d['pm2_5'], datetime_id=db_datetime.id, sensor_id=db_obj_sensor.id)
+                except Air.DoesNotExist:
+                    db_pm2_5 = Air(pm2_5=d['pm2_5'], datetime_id=db_datetime.id, sensor_id=db_obj_sensor.id)
+                    db_pm2_5.save()
+        else:
+            data['text'] = {'message':'no such sensor'}
+        data["text"] = "Response to POST request."
         return JsonResponse(data, status=200, safe=True)
     
     else: # GET
         data["text"] = "Response to GET request."
-        monitor_sn = request.GET.get('monitor')
-        db_obj_monitor = Sensor.objects.filter(serial_number=monitor_sn).first()
-        if (db_obj_monitor != None):
-            db_obj_pm2_5 = list(Air.objects.filter(monitor_id=db_obj_monitor.id).values())
+        sensor_serial_number = request.GET.get('sensor_serial_number')
+        db_obj_sensor = Sensor.objects.filter(serial_number=sensor_serial_number).first()
+        if (db_obj_sensor != None):
+            db_obj_pm2_5 = list(Air.objects.filter(sensor_id=db_obj_sensor.id).values())
             pm2_5 = [o['pm2_5'] for o in db_obj_pm2_5]
             datetime_ids = [o['datetime_id'] for o in db_obj_pm2_5]
             dts = [o['datetime'].strftime("%Y-%m-%d %H:%M:%S") for o in DateTime.objects.filter(id__in=datetime_ids).values()]
