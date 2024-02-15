8715ffc1df3b2a99c12481a338973c9752063b8e
Sahil <lunawatsahil@gmail.com>
2024-02-15 14:23:17 +0000

    Update to prediction API

--- SCM/Backend/dublinBikesPrediction/views.py
+++ SCM/Backend/dublinBikesPrediction/views.py
@@ -12,8 +12,9 @@ class DublinBikesModel:
             cls._instance = super(DublinBikesModel, cls).__new__(cls)
             # Initialize your variable here to ensure it's only done once.
             print('Load Dublin Bikes Usage predictor model')
-            cls.model = pickle.load(open("dublinBikesPrediction/assets/Resources/RFmodel.pkl", 'rb'))
+            cls.model = pickle.load(open("dublinBikesPrediction/assets/RFmodel_lr.pkl", 'rb'))
             cls.df_stations = pd.read_csv('dublinBikesPrediction/assets/STATION ID - BIKE STANDS.csv')
+            cls.df_positions = pd.read_csv('dublinBikesPrediction/assets/STATION ID - POSITION.csv')
             print('Model load complete')
         return cls._instance
 
@@ -23,6 +24,12 @@ class DublinBikesModel:
 
     def get_model(self):
         return DublinBikesModel.model
+
+    def get_df_stations(self):
+        return DublinBikesModel.df_stations
+
+    def get_df_positions(self):
+        return DublinBikesModel.df_positions
 
 dublinBikesModel = DublinBikesModel()
 
@@ -47,24 +54,33 @@ def prepare_query_for_model(weather_forecast=None):
     res = res[['date', 'precipitation.total', 'temperature']]
     res.rename(columns={'precipitation.total': 'rain', 'temperature': 'temp', 'date': 'TIME'}, inplace=True)
     
+    
     res['hour'] = res['TIME'].dt.hour
     res['minute'] = res['TIME'].dt.minute
-    res['month'] = res['TIME'].dt.month
     res['day'] = res['TIME'].dt.day
     res['dayofweek'] = res['TIME'].dt.dayofweek
 
-    res.drop('TIME', axis=1, inplace=True)
 
-    df_stations = pd.read_csv('dublinBikesPrediction/assets/STATION ID - BIKE STANDS.csv')
-    res['dummy'] = 1
-    df_stations['dummy'] = 1
-    res = pd.merge(df_stations, res, on='dummy')
-    res = res.drop(columns='dummy')
+    df_stations = dublinBikesModel.get_df_stations()
+    res = merge_dfs_on_stations(df_stations, res)
     res = res.reset_index(drop=True)
-    return res
+    df_return = pd.DataFrame()
+    df_return = res.copy()
+    df_return.drop(['hour', 'minute', 'day', 'dayofweek'], axis=1, inplace=True)
+    res.drop('TIME', axis=1, inplace=True)
+    return res, df_return
+
+def merge_dfs_on_stations(df1, df2):
+    df1['dummy'] = 1
+    df2['dummy'] = 1
+    df2 = pd.merge(df1, df2, on='dummy')
+    df2 = df2.drop(columns='dummy')
+    return df2
 
 def predict(request):
-    data = prepare_query_for_model()
-    prediction = dublinBikesModel.get_model().predict(data)
-    data['prediction'] = prediction
-    return JsonResponse({'prediction': data.to_json()})
+    df_query, df_return = prepare_query_for_model()
+    prediction = dublinBikesModel.get_model().predict(df_query)
+    df_return['prediction'] = prediction
+    positions = dublinBikesModel.get_df_positions()
+    print(df_return.columns)
+    return JsonResponse({'prediction': df_return.to_json(), 'positions': positions.to_json()})
