1d2b725d70fe1ac5875752ad4069e52e5fd74a23
Smit Shah <smit16@somaiya.edu>
2024-02-21 13:14:06 +0000

    SCM-16: Added Dublin bus backend (#20)
    * SCM-16: Added Dublin bus backend
    
    * SCM-16: Fixing authentication migrations
    
    * SCM-16: Added requirements in/txt files

--- SCM/Backend/Backend/settings.py
+++ SCM/Backend/Backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from corsheaders.defaults import default_headers
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,13 +30,22 @@ SECRET_KEY = "django-insecure-$7z!5(9-iym3&qu^3^dmz_sc1xx02mrd4iog6gy@soja=d9qls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'http:localhost:3000']
+ALLOWED_HOSTS = ['*', 'http://localhost:3000']
 
 CSRF_TRUSTED_ORIGINS = ['https://fa80-134-226-214-245.ngrok-free.app']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# TODO: Remove this
+CORS_ALLOW_HEADERS = default_headers + (
+    'ngrok-skip-browser-warning',
+)
+
+# TODO: Modify
+CSRF_TRUSTED_ORIGINS = ['https://cce2-2a02-8084-2561-8e80-2c1c-d52d-f7ef-da30.ngrok-free.app/']
+
 # CSRF_COOKIE_SECURE = False
 
 # Application definition
@@ -44,9 +58,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "authentication",
+    "corsheaders",
     "django_extensions",
+
+    "authentication",
     "sensors",
+    "cityservices",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +152,27 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    "formatters": {
+        "default": {
+            "format": "[{asctime}]:{levelname}:{name}: {message}",
+            "style": "{",
+        },
+    },
+    'handlers': {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            'propagate': True,
+        },
+    },
+}
