4d80606b0f3cf4c19458f5c56b88d565516ad9dc
Sahil <lunawatsahil@gmail.com>
2024-03-15 02:09:26 +0000

    Added dublin waste facilities locations

--- SCM/Frontend/src/Components/BinLocations.js
+++ SCM/Frontend/src/Components/BinLocations.js
@@ -1,37 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { FaBars, FaBus, FaVolumeUp, FaWind, FaTrash } from "react-icons/fa";
-
-// import 'leaflet.markercluster/dist/MarkerCluster.css';
-// import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import "leaflet.markercluster";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BASE_URL } from "../services/api";
+import { BASE_URL, DUBLIN_WASTE_FACILITIES } from "../services/api";
+import binIconPng from "../assets/bin_icon.png";
 
-import bikeImage from "../assets/bin_icon.png";
-const icon = L.icon({
+const binIcon = L.icon({
 	iconSize: [22, 22],
 	iconAnchor: [11, 11],
 	popupAnchor: [0, -11],
-	iconUrl: bikeImage,
+	iconUrl: binIconPng,
 });
 
-const BinLocations = () => {
+export default function BinLocations() {
 	const [map, setMap] = useState(null);
-	const [binData, setBinData] = useState({});
+	const [mapLayersControl, setMapLayersControl] = useState(null);
+	const [binData, setBinData] = useState(null);
+	const [wasteFacilities, setWasteFacilities] = useState(null);
+	var markersLayer1 = useRef(L.layerGroup());
+	var markersLayer2 = useRef(L.layerGroup());
 
+	function initializeMap() {
+		if (!map) {
+			const leafletMap = L.map("map", {
+				center: [53.349805, -6.26031],
+				zoom: 13,
+			});
+			L.tileLayer(
+				"https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png",
+				{ attribution: "leaflet" }
+			).addTo(leafletMap);
+
+			setMapLayersControl(L.control.layers(null).addTo(leafletMap));
+
+			setMap(leafletMap);
+		}
+	}
+
+	// Fetch the data
 	useEffect(() => {
+		// Fetch bin locations and store in variable `binData`
 		fetch(`${BASE_URL}/city_services/bin-locations/`)
 			.then((response) => {
 				if (response.status === 200) return response.json();
-				else throw new Error("Fetching dublin bikes prediction data failed!");
+				else throw new Error("Fetching bin location data failed!");
 			})
 			.then((data) => {
-				console.log("fetched data", data);
 				setBinData(data.data);
+			})
+			.catch((error) => {
+				toast.dark(error.toString());
+				console.log(error);
+			});
+
+		// Fetch waste facilities locations and store in variable `wasteFacilities`
+		fetch(DUBLIN_WASTE_FACILITIES)
+			.then((response) => {
+				if (response.status === 200) return response.json();
+				else throw new Error("Fetching waste facilities data failed!");
+			})
+			.then((data) => {
+				setWasteFacilities(data);
 			})
 			.catch((error) => {
 				toast.dark(error.toString());
@@ -40,13 +71,15 @@ const BinLocations = () => {
 	}, []);
 
 	useEffect(() => {
-		console.log("binData", binData);
-		if (map && Object.keys(binData).length !== 0) {
-			const markers = L.markerClusterGroup({
+		initializeMap();
+		if (map && binData && !map.hasLayer(markersLayer1.current)) {
+			markersLayer1.current = L.markerClusterGroup({
 				iconCreateFunction: function (cluster) {
 					return L.divIcon({
 						html:
-							'<div style="background-color: grey; color: white; border-radius: 50%; width: 30px; height: 30px; display: flex; justify-content: center; align-items: center;">' +
+							`<div style="background-color: grey; color: white; 
+							border-radius: 50%; width: 30px; height: 30px; display: 
+							flex; justify-content: center; align-items: center;">` +
 							cluster.getChildCount() +
 							"</div>",
 						className: "custom-cluster-icon",
@@ -61,28 +94,45 @@ const BinLocations = () => {
 			binData.features.forEach((bin) => {
 				const marker = L.marker(
 					[bin.geometry.coordinates[1], bin.geometry.coordinates[0]],
-					{ icon: icon }
-				);
+					{ icon: binIcon }
+				).addTo(markersLayer1.current);
 				marker.bindPopup(
 					`<b>${bin.properties.Electoral_Area}</b><br>Bin Type: ${bin.properties.Bin_Type}`
 				);
-				markers.addLayer(marker);
 			});
-
-			map.addLayer(markers);
+			map.addLayer(markersLayer1.current);
+			mapLayersControl.addOverlay(markersLayer1.current, "Bins");
 		}
-		if (!map) {
-			const leafletMap = L.map("map").setView([53.349805, -6.26031], 13);
-			L.tileLayer(
-				"https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png",
-				{
-					attribution: "© OpenStreetMap contributors",
-				}
-			).addTo(leafletMap);
 
-			setMap(leafletMap);
+		if (map && wasteFacilities && !map.hasLayer(markersLayer2.current)) {
+			wasteFacilities.features.forEach(function (wasteFacility) {
+				// Keeping facilities in Dublin only
+				if (
+					wasteFacility.geometry.coordinates[0][1] > 53.45 ||
+					wasteFacility.geometry.coordinates[0][1] < 53.19  ||
+					wasteFacility.geometry.coordinates[0][0] < -6.45122489
+				)
+					return;
+
+				console.log(wasteFacility.properties);
+				console.log(wasteFacility.properties.Category);
+				var marker = L.marker([
+					wasteFacility.geometry.coordinates[0][1],
+					wasteFacility.geometry.coordinates[0][0],
+				]).addTo(markersLayer2.current);
+				marker.bindPopup(
+					`<h1><b>${wasteFacility.properties.Name}</b></h1>
+					<br/><b>Address:</b> ${wasteFacility.properties.Address}
+					<br/><b>Licence Number:</b> ${wasteFacility.properties.ActiveLicenceNumber}
+					<br/><b>Location:</b> ${wasteFacility.geometry.coordinates[0][1]},
+						${wasteFacility.geometry.coordinates[0][0]}
+					`
+				);
+			});
+			map.addLayer(markersLayer2.current);
+			mapLayersControl.addOverlay(markersLayer2.current, "Waste Facilities");
 		}
-	}, [map, binData]);
+	}, [map, binData, wasteFacilities]);
 
 	return (
 		<div>
@@ -110,6 +160,4 @@ const BinLocations = () => {
 			/>
 		</div>
 	);
-};
-
-export default BinLocations;
+}
