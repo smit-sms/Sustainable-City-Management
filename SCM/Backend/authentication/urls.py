46594a3712cdba69b5a9d1d6c8ee74b8c42c60ab
Sahil <lunawatsahil@gmail.com>
2024-04-11 16:32:51 +0100

    SCM-22: Added authentication login test

--- SCM/Backend/authentication/urls.py
+++ SCM/Backend/authentication/urls.py
@@ -2,9 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path("login",views.Authenticate.as_view()),
-    path("signup",views.Signup.as_view()),
-    path("whitelist",views.New_whitelist.as_view()),
-    path("get",views.get_user.as_view()),
-    path("is_authenticated",views.IsAuthenticated.as_view())
+    path("login",views.Authenticate.as_view(), name="authentication_login"),
+    path("signup",views.Signup.as_view(), name="authentication_signup"),
+    path("whitelist",views.New_whitelist.as_view(), name="authentication_whitelist"),
+    path("get",views.get_user.as_view(), name="authentication_get"),
+    path("is_authenticated",views.IsAuthenticated.as_view(), name="authentication_is_authenticated")
 ]
