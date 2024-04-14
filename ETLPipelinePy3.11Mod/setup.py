dd8d6fbc5c247cfdf9d5ef3f0213a7bec8c11dd7
Gayathri Girish Nair <ggnair2000@gmail.com>
2024-04-14 20:12:14 +0100

    python version 3.11.5 code modularized

--- ETLPipelinePy3.11Mod/setup.py
+++ ETLPipelinePy3.11Mod/setup.py
@@ -1,13 +1,19 @@
 import setuptools
 
+long_description = """
+    This package facilitates
+"""
+
 setuptools.setup(
     name='etl_pipeline_ggn1_ase_g5',
-    version='3',
+    version='4.0',
     packages=setuptools.find_packages(),
-    install_requires=['dill', 'fastapi', 'schedule', 'uvicorn'],
+    install_requires=['dill', 'fastapi', 'schedule', 'uvicorn', 'pandas'],
     entry_points={
         'console_scripts': [
             'etl_pipeline = etl_pipeline_ggn1_ase_g5:__main__',
         ]
-    }
+    },
+    description='Short description',
+    long_description=long_description
 )
