2261b323738f7c830d4ab0bd7a85efcdbee5c05a
Sahil <lunawatsahil@gmail.com>
2024-04-15 18:29:58 +0100

    Added backend tests

--- SCM/Backend/authentication/tests.py
+++ SCM/Backend/authentication/tests.py
@@ -1,104 +1,79 @@
-from django.test import TestCase
-from .models import User,Whitelist
-from .views import *
-from requests import get,post 
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import User, Whitelist
 
+class TestWhitelist(APITestCase):
+    def setUp(self):
+        self.url = reverse('whitelist')
+        self.email = 'testuser@tcd.ie'
 
-class UserTestCase(TestCase):
-    test_use="sahil1"
-    test_password="12345678"
-    #need to change the test email after each test because the update test change the password and username associated to the email and so the get request won't get anything with the previous query
-    test_email="sahil@te.ie"
-    new_password="67891"
-    new_username="Smarann"
+    def test_new_whitelist(self):
+        data = {'email': self.email}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, "New email added to Whitelist")
+    
+    def test_duplicate_whitelist(self):
+        Whitelist.objects.create(email=self.email)
+        data = {'email': self.email}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_208_ALREADY_REPORTED)
+        self.assertEqual(response.data, "Already existing email")
 
-    auth_test_use="JP"
-    auth_test_password="mdp"
-    auth_test_email="dragon123@tcd.ie"
+class TestAuth(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+        self.email = 'testuser@tcd.ie'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(email=self.email, password=self.password)
+        Whitelist.objects.create(email='whitelist-test@tcd.ie')
 
-    def test_signup(self):
-        test_use=self.test_use
-        test_password=self.test_password
-        test_email=self.test_email
-        
-        whitelist_add=post('http://127.0.0.1:8000/authentication/whitelist',data={
-            "email":test_email
-        })
-        signup_call=post('http://127.0.0.1:8000/authentication/signup',data={
-            "username":test_use,
-            "email":test_email,
-            "password":test_password
-        })
-        user=post('http://127.0.0.1:8000/authentication/get',data={
-            "username":test_use,
-            "email":test_email,
-            "password":test_password     
-                  })
-        dic=user.json()
-        print(dic)
-        print(signup_call.json())
-        l=[dic["username"],dic["email"],dic["password"]]
-        self.assertEqual([test_use,test_email,test_password],l)
+    def test_register(self):
+        data = {'email': 'whitelist-test@tcd.ie', 'password': 'newuserpassword', 'name': 'New User'}
+        response = self.client.post(self.register_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('access_token', response.data)
+        self.assertIn('refresh_token', response.data)
 
-    def test_authenticate(self):
-        test_use=self.auth_test_use
-        test_password=self.auth_test_password
-        test_email=self.auth_test_email
+    def test_login(self):
+        data = {'email': self.email, 'password': self.password}
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access_token', response.data)
+        self.assertIn('refresh_token', response.data)
 
-        whitelist_add=post('http://127.0.0.1:8000/authentication/whitelist',data={
-            "email":test_email
-        })
-        signup_call=post('http://127.0.0.1:8000/authentication/signup',data={
-            "username":test_use,
-            "email":test_email,
-            "password":test_password
-        })
+    def test_logout(self):
+        data = {'email': self.email, 'password': self.password}
+        response = self.client.post(reverse('token_obtain_pair'), data)
+        token = response.json()['refresh']
+        data = {'refresh_token': str(token)}
+        response = self.client.post(self.logout_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"message": "Successfully Logged out!"})
 
-        user=post('http://127.0.0.1:8000/authentication/authenticate',data={
-        "username":test_use,
-        "email":test_email,
-        "password":test_password     
-                })
-        response=user.json()
-        print(response)
-        self.assertEqual("Successful login",response)
+class TestTokenEndpoints(APITestCase):
+    def setUp(self):
+        Whitelist(email='testuser@tcd.ie').save()
+        User.objects.create_user(email='testuser@tcd.ie', password='testpassword')
 
-    def test_update(self):
-        test_use=self.test_use
-        test_password=self.test_password
-        test_email=self.test_email
+    def test_token_obtain_pair(self):
+        url = reverse('token_obtain_pair')
+        data = {'email': 'testuser@tcd.ie', 'password': 'testpassword'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
 
-        update=post('http://127.0.0.1:8000/authentication/update',data={
-        "username":test_use,
-        "email":test_email,
-        "password":test_password,
-        "new_password":self.new_password,
-        "new_username":self.new_username     
-                })
-        response=update.json()
-        print(response)
-        user=post('http://127.0.0.1:8000/authentication/get',data={
-            "username":self.new_username,
-            "email":test_email,
-            "password":self.new_password     
-                  })
-        dic=user.json()
-        print(dic)
-        l=[dic["username"],dic["email"],dic["password"]]
-        self.assertEqual([self.new_username,test_email,self.new_password],l)
+    def test_token_refresh(self):
+        data = {'email': 'testuser@tcd.ie', 'password': 'testpassword'}
+        response = self.client.post(reverse('token_obtain_pair'), data)
+        refresh_token = response.json()['refresh']
 
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
+        url = reverse('token_refresh')
+        data = {'refresh': refresh_token}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
